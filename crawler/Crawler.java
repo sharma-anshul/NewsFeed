8a757d758d5236e675d16d19959774a4c96aa975
sharma-anshul <ansharma@seas.upenn.edu>
2013-08-13 14:50:06 -0400

    Added method to get internal links

--- crawler/Crawler.java
+++ crawler/Crawler.java
@@ -4,9 +4,12 @@
  *
  */
 
-import java.util.*;
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import de.l3s.boilerpipe.extractors.DefaultExtractor;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
@@ -16,7 +19,8 @@ public class Crawler {
 						String url = args[0].startsWith("http://") ? args[0] : "http://" + args[0];
 						String page = getPage(url);
 						String text = getText(page);
-						System.out.println(text);
+						ArrayList<String> internalLinks = getInternalLinks(page);
+						//System.out.println(internalLinks);
 				} catch(IOException e) {
 						e.printStackTrace();
 				} catch(Exception e) {
@@ -44,5 +48,18 @@ public class Crawler {
 				String text = extractor.getText(page);
 
 				return text;
+		}
+		
+		//Uses regex to extract all links on a page
+		public static ArrayList<String> getInternalLinks(String page) {
+				ArrayList<String> internalLinks = new ArrayList<String>();
+				Pattern pattern = Pattern.compile("<(a|A) (href|HREF)=\"(.*?)\".*?>.*?</(\\1)>");
+				Matcher matcher = pattern.matcher(page);
+
+				while(matcher.find()) {
+						internalLinks.add(matcher.group(3));
+				}
+
+				return internalLinks;
 		}
 }
