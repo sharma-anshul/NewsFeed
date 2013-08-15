6f7888eae93635957776e9b59eb937053cf52ba2
sharma-anshul <ansharma@seas.upenn.edu>
2013-08-15 15:00:18 -0400

    Added method for Topic Analysis

--- summarizer/Summarizer.java
+++ summarizer/Summarizer.java
@@ -4,11 +4,37 @@
  *
  */
 
-import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Summarizer {
-
 		public static void main(String[] args) {
-				System.out.println("Yo bitch wassup");
+				try {
+						getTopicWords();
+				} catch(IOException e) {
+						e.printStackTrace();
+				} catch(Exception e) {
+						e.printStackTrace();
+				}
+		}
+
+		//Performs Topic Analysis on article to get Topic words
+		public static HashMap<String, Double> getTopicWords() throws Exception {
+				Process p = Runtime.getRuntime().exec("./Topic.sh");
+				p.waitFor();
+
+				File ts = new File("../TopicWordTool/TopicWords-v2/test.ts");
+				BufferedReader in = new BufferedReader(new FileReader(ts));
+				HashMap topicWords = new HashMap<String, Double>();
+				String line;
+				while((line = in.readLine()) != null) {
+						String word = line.split("\\s")[0];
+						double score = Double.parseDouble(line.split("\\s")[1]);
+
+						topicWords.put(word, score);
+				}
+
+				return topicWords;
 		}
 }
---
+++ summarizer/Topic.sh
@@ -0,0 +1,3 @@
+cd ../TopicWordTool/TopicWords-v2
+
+java -Xmx1000m TopicSignatures config.example
