85ca6439629d717f4a30966058e12aefd9cec4b3
sharma-anshul <ansharma@seas.upenn.edu>
2013-08-15 12:22:45 -0400

    Added function to specify crawl limit

--- crawler/Crawler.java
+++ crawler/Crawler.java
@@ -1,12 +1,12 @@
 /* Usage:
  *
- * ?> java Crawler URL 
+ * ?> java Crawler URL LIMIT
  *
  */
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -17,15 +17,36 @@ public class Crawler {
 		public static void main(String[] args) {
 				try {
 						String url = args[0].startsWith("http://") ? args[0] : "http://" + args[0];
-						String page = getPage(url);
-						String text = getText(page);
-						ArrayList<String> internalLinks = getInternalLinks(page);
-						//System.out.println(internalLinks);
+						int limit = Integer.parseInt(args[1]);
+
+						HashSet<String> links = getLinks(url, limit);
+						
+						for (String link: links) {
+								System.out.println(link);
+						}
+
 				} catch(IOException e) {
 						e.printStackTrace();
 				} catch(Exception e) {
 						e.printStackTrace();
 				}
+		}
+		
+		//Given a link, get n more links from the same domain
+		public static HashSet<String> getLinks(String url, int limit) throws Exception {
+				HashSet<String> links = new HashSet<String>();
+				HashSet<String> temp = new HashSet<String>();
+				String page, text, link = url;
+				
+				while (links.size() < limit) {
+						page = getPage(link);
+						HashSet<String> internalLinks = getInternalLinks(page);
+						links.addAll(internalLinks);
+
+						link = (String) links.toArray()[links.size() - 1];
+				}
+
+				return links;
 		}
 
 		//Downloads page and returns String representation
@@ -51,8 +72,8 @@ public class Crawler {
 		}
 		
 		//Uses regex to extract all links on a page
-		public static ArrayList<String> getInternalLinks(String page) {
-				ArrayList<String> internalLinks = new ArrayList<String>();
+		public static HashSet<String> getInternalLinks(String page) {
+				HashSet<String> internalLinks = new HashSet<String>();
 				Pattern pattern = Pattern.compile("<(a|A) (href|HREF)=\"(.*?)\".*?>.*?</(\\1)>");
 				Matcher matcher = pattern.matcher(page);
 
@@ -61,5 +82,23 @@ public class Crawler {
 				}
 
 				return internalLinks;
+		}
+
+		//Select links with a length greater than the average link length
+		public static ArrayList<String> getLongLinks(ArrayList<String> links) {
+				ArrayList<String> longLinks = new ArrayList<String>();
+				double avgLen = 0.0;
+				for (String link: links) {
+						avgLen += link.length();
+				}
+				avgLen /= links.size();
+
+				for (String link: links) {
+						if (link.length() > avgLen) {
+								longLinks.add(link);
+						}
+				}
+
+				return longLinks;
 		}
 }
